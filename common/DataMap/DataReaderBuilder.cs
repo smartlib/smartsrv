80a34940b355180e0d55f6fad4baf5edaa4f1257
ulyanov yuriy <ulyanov.yuriy@live.com>
2017-06-21 00:51:08 +0300

    map

---
+++ common/DataMap/DataReaderBuilder.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace DataMap
+{
+    public class DataReaderBuilder<T> : IDisposable
+    {
+        private Dictionary<string, MemberInfo> _maps;
+
+        public DataReaderBuilder()
+        {
+            _maps = new Dictionary<string, MemberInfo>();
+        }
+
+        public IDictionary<string, MemberInfo> Maps
+        {
+            get
+            {
+                if (_maps == null) throw new ObjectDisposedException(nameof(Maps));
+                return _maps;
+            }
+        }
+
+        public DataReaderBuilder<T> AddMap(MemberInfo source, string destination)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (destination == null) throw new ArgumentNullException(nameof(destination));
+
+            Maps.Add(destination, source);
+
+            return this;
+        }
+
+        private static MemberInfo GetMemberInfo<P>(Expression<Func<T, P>> get)
+        {
+            if (get == null) throw new ArgumentNullException(nameof(get));
+
+            MemberExpression exp = null;
+            if (get.Body.NodeType == ExpressionType.Convert)
+                exp = ((UnaryExpression)get.Body).Operand as MemberExpression;
+            else
+                exp = get.Body as MemberExpression;
+            if (exp == null) throw new ArgumentException($"invalid expression {get}");
+
+            return exp.Member;
+        }
+
+        public void Dispose()
+        {
+            _maps = null;
+        }
+    }
+}
---
+++ db/StockDb/dbo/User Defined Types/Price.sql
@@ -0,0 +1,8 @@
+﻿CREATE TYPE [dbo].[Price] AS TABLE (
+    [DateTime] DATETIME   NOT NULL,
+    [Open]     FLOAT (53) NOT NULL,
+    [Max]      FLOAT (53) NOT NULL,
+    [Min]      FLOAT (53) NOT NULL,
+    [Close]    FLOAT (53) NOT NULL,
+    PRIMARY KEY CLUSTERED ([DateTime] ASC));
+
